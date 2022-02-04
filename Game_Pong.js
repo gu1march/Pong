8eb0f05b34c1ed63a0f74120e4e01ce977f5bddc
gu1march <98661325+gu1march@users.noreply.github.com>
2022-02-03 21:09:29 -0300

    Create Game_Pong.js

---
+++ Game_Pong.js
@@ -0,0 +1,72 @@
+//Here is the game pong in js, it's not finished yet but soon it will be.
+//variáveis da bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 21;
+let raio = diametro / 2 ;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+let raqueteComprimento = 10;
+let raqueteAltura = 70;
+
+//Variáveis da Raquete
+let xRaquete = 5;
+let yRaquete = 150;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete();
+  movimentaMinhaRaquete();
+  verificaColisaoRaquete();
+  
+}
+
+function mostraBolinha(){
+  circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha(){
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda(){
+  if (xBolinha + raio> width ||
+     xBolinha - raio< 0){
+    velocidadeXBolinha *= -1;
+  }
+  if (yBolinha + raio> height ||
+     yBolinha - raio < 0){
+    velocidadeYBolinha *= -1;
+  }
+}
+
+function mostraRaquete(){
+  rect(xRaquete, yRaquete, raqueteComprimento, raqueteAltura);
+}
+
+function movimentaMinhaRaquete(){
+  if (keyIsDown(UP_ARROW)){
+    yRaquete -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    yRaquete += 10;
+  }
+}
+
+function verificaColisaoRaquete(){
+  if (xBolinha - raio < xRaquete + raqueteComprimento
+     && yBolinha - raio < yRaquete + raqueteAltura)
+  {
+    velocidadeXBolinha *= -1;
+  }
+}
